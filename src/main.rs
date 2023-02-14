9fb037d32714b4d6dbe693fe1cd2dce57200b7a2
Tam <hi@tam.sx>
2023-02-14 22:22:00 +0000

    Update test file & add debug dump

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,46 @@ extern crate pest;
 extern crate pest_derive;
 
 use std::fs;
+use pest::iterators::Pair;
 use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "pgsl.pest"]
-pub struct PGSLParser;
+struct PGSLParser;
 
 fn main() {
-	let unparsed_file = fs::read_to_string("test.pgl").expect("failed to read file");
+	debug();
+}
+
+fn debug() {
+	let unparsed_file = fs::read_to_string("test.pgl")
+		.expect("failed to read file");
+
 	let file = PGSLParser::parse(Rule::pgsl, &unparsed_file)
 		.expect("failed to parse")
 		.next().unwrap();
 
-	println!("{:?}", file);
+	println!("{:?}", file.as_rule());
+
+	for line in file.into_inner() {
+		debug_walk(line, 1);
+	}
+}
+
+fn debug_walk(pair : Pair<Rule>, depth : usize) {
+	if pair.as_rule() == Rule::EOI { return; }
+
+	let rule = pair.as_rule();
+	let value = pair.as_str();
+	let mut pairs = pair.into_inner().peekable();
+
+	if pairs.peek().is_some() {
+		println!("{:indent$}{:?}", "", rule, indent = depth * 2);
+	} else {
+		println!("{:indent$}{:?}: {}", "", rule, value, indent = depth * 2);
+	}
+
+	for line in pairs {
+		debug_walk(line, depth + 1);
+	}
 }
