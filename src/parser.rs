dd58f2b7a65409e0dd3dfd9f0f8a86259ffbdd0d
Tam <hi@tam.sx>
2023-02-16 09:29:38 +0000

    table -> struct

--- src/parser.rs
+++ src/parser.rs
@@ -18,6 +18,14 @@ pub struct PGSLColumn {
 }
 
 #[derive(Debug, Default)]
+pub struct PGSLTable {
+	pub name : String,
+	pub schema : String,
+	pub extends : Vec<String>,
+	pub columns : Vec<PGSLColumn>,
+}
+
+#[derive(Debug, Default)]
 pub struct PGSLInterface {
 	pub name : String,
 	pub columns : Vec<PGSLColumn>,
@@ -27,6 +35,7 @@ pub struct PGSLInterface {
 pub struct PGSLData {
 	pub requires : Vec<PathBuf>,
 	pub interfaces : HashMap<String, PGSLInterface>,
+	pub tables : Vec<PGSLTable>,
 }
 
 /// Parses the files at the given path into PGSLData
@@ -36,12 +45,14 @@ pub fn parse (path : PathBuf) -> Result<PGSLData> {
 		.with_context(|| format!("Unable to read file {path_as_str}"))?;
 
 	let file = PGSLParser::parse(Rule::pgsl, &unparsed_file)
-		.with_context(|| format!("Failed to parse {path_as_str}"))?.next().unwrap();
+		.with_context(|| format!("Failed to parse {path_as_str}"))?
+		.next().unwrap();
 
 	let mut data = PGSLData::default();
 
 	for line in file.into_inner() {
 		match line.as_rule() {
+			Rule::EOI => (),
 			Rule::require => {
 				let mut requires = parse_require(line);
 				data.requires.append(&mut requires);
@@ -50,7 +61,11 @@ pub fn parse (path : PathBuf) -> Result<PGSLData> {
 				let interface = parse_interface(line);
 				data.interfaces.insert(interface.name.clone(), interface);
 			},
-			_ => (),
+			Rule::table => {
+				let table = parse_table(line);
+				data.tables.push(table);
+			},
+			_ => unreachable!(),
 		}
 	}
 
@@ -64,7 +79,7 @@ fn parse_require (lines : Pair<Rule>) -> Vec<PathBuf> {
 	for line in lines.into_inner() {
 		if line.as_rule() == Rule::path {
 			requires.push(PathBuf::from(line.as_str()));
-		}
+		} else { unreachable!() }
 	}
 
 	requires
@@ -85,6 +100,36 @@ fn parse_interface (lines : Pair<Rule>) -> PGSLInterface {
 	interface
 }
 
+/// Parses the table rule
+fn parse_table (lines : Pair<Rule>) -> PGSLTable {
+	let mut table = PGSLTable::default();
+
+	for line in lines.into_inner() {
+		match line.as_rule() {
+			Rule::schema_name => table.schema = line.as_str().to_string(),
+			Rule::table_name => table.name = line.as_str().to_string(),
+			Rule::columns => table.columns = parse_columns(line),
+			Rule::extends => table.extends = parse_extends(line),
+			_ => unreachable!(),
+		}
+	}
+
+	table
+}
+
+/// Parses the extends rule
+fn parse_extends (lines : Pair<Rule>) -> Vec<String> {
+	let mut names = Vec::new();
+
+	for line in lines.into_inner() {
+		if line.as_rule() == Rule::interface_name {
+			names.push(line.as_str().to_string());
+		} else { unreachable!() }
+	}
+
+	names
+}
+
 /// Parses the columns rule
 fn parse_columns (lines : Pair<Rule>) -> Vec<PGSLColumn> {
 	let mut columns = Vec::new();
@@ -92,7 +137,7 @@ fn parse_columns (lines : Pair<Rule>) -> Vec<PGSLColumn> {
 	for line in lines.into_inner() {
 		if line.as_rule() == Rule::column {
 			columns.push(parse_column(line));
-		}
+		} else { unreachable!() }
 	}
 
 	columns
@@ -108,7 +153,7 @@ fn parse_column (lines : Pair<Rule>) -> PGSLColumn {
 			Rule::type_name => column.type_name = line.as_str().to_string(),
 			Rule::column_attributes => column.attributes = line.as_str().to_string(),
 			Rule::column_comment => column.comments.push(line.as_str().to_string()),
-			_ => unreachable!()
+			_ => unreachable!(),
 		}
 	}
 
